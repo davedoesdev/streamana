7ec6ef4d471a0cb3a394263487fa60bef0212243
David Halls <dahalls@gmail.com>
2021-10-06 07:58:32 +0100

    Use npx to run server

--- scripts/run_http_server.sh
+++ scripts/run_http_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")/../site"
 if test "$1" = "--https"; then
-  serve -S -l tcp://0.0.0.0 --ssl-cert ../certs/server.crt --ssl-key ../certs/server.key
+  npx serve -S -l tcp://0.0.0.0 --ssl-cert ../certs/server.crt --ssl-key ../certs/server.key
 else
-  serve -S -l tcp://0.0.0.0
+  npx serve -S -l tcp://0.0.0.0
 fi
